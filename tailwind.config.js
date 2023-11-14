e82807c0bce3d7ba76b11f0e1db552d5c82c9b75
fedeval98 <fede.val9806@gmail.com>
2023-11-14 08:54:14 -0300

    subida de ejercicios

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          spaceCadet: '#7052FF',
+          ultraViolet: '#d06aff',
+          eggplant: '#FF81FF',
+          mauve: '#957186',
+        }
+      }
+    }
+  }
